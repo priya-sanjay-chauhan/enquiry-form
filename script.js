84cefd83ba097d2d5709514600630416bc6f7630
Priya Chauhan <Priya@123>
2024-02-03 23:35:27 +0530

    created

---
+++ script.js
@@ -0,0 +1,13 @@
+let btn=document.querySelector("button");
+let name=document.querySelector("#name");
+let mob=document.querySelector("#mob");
+let enq=document.querySelector("#enq");
+// btn.addEventListener("click",function(e){
+//     console.log(name.value);
+//     console.log(mob.value);
+//     console.log(enq.value);
+// })
+name.addEventListener("click",function(e){
+    console.log(e);
+})
+
